666928cce3c4fdd8016ad0c4dafc2701ef2d135e
MarcelinaLukasik <98186787+MarcelinaLukasik@users.noreply.github.com>
2023-04-07 13:53:43 +0200

    Update Program.cs
    refactor

--- BattleshipWithForms/Program.cs
+++ BattleshipWithForms/Program.cs
@@ -2,9 +2,6 @@ namespace BattleshipWithForms
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
